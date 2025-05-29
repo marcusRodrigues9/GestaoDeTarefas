bc2ee3de3556d78a3277bc9f461b9c544133c4dc
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-05-28 22:20:04 -0300

    first commit

---
+++ src/br/com/poo/gestaotarefas/task/Task.java
@@ -0,0 +1,6 @@
+package br.com.poo.gestaotarefas.task;
+
+public class Task {
+    private String name;
+    private String day;
+}
---
+++ src/br/com/poo/gestaotarefas/test/MainTeste.java
@@ -0,0 +1,7 @@
+package br.com.poo.gestaotarefas.test;
+
+public class MainTeste {
+    public static void main(String[] args) {
+        
+    }
+}
