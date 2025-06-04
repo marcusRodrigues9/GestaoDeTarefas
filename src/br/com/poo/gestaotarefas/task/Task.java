a0741083785cd997c86814a852c855d921096c43
marcusRodrigues9 <marcus.rodriguescv@gmail.com>
2025-06-03 22:25:46 -0300

    adicionado atributos na classe tarefas

--- src/br/com/poo/gestaotarefas/task/Task.java
+++ src/br/com/poo/gestaotarefas/task/Task.java
@@ -1,6 +1,15 @@
 package br.com.poo.gestaotarefas.task;
 
 public class Task {
+    private int id;
     private String name;
     private String day;
+    private boolean completed;
+
+    public Task(int id, String name, String day, boolean completed) {
+        this.id = id;
+        this.name = name;
+        this.day = day;
+        this.completed = completed;
+    }
 }
